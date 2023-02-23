c3e51a3e11d114c1d14593cb28acd45cd613172a
tatiSmol <judeclapton@icloud.com>
2023-02-23 22:35:14 +0300

    added 2 new templates and blocks of header and footer; plus new class with blog controller and plus one method to Main with mapping "about" page.

--- src/main/java/com/tatiSmol/blog/controllers/MainController.java
+++ src/main/java/com/tatiSmol/blog/controllers/MainController.java
@@ -12,4 +12,10 @@ public class MainController {
         return "home";
     }
 
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About us Page");
+        return "about";
+    }
+
 }
